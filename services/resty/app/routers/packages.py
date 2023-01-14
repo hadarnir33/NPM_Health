bffe051e802bdca0a511a7978afecd8c3aaf2e72
Hadar Nir <hadarnir33@gmail.com>
2023-01-14 21:01:53 +0200

    Add data model to router

--- services/resty/app/routers/packages.py
+++ services/resty/app/routers/packages.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
 import bl
+import models
 
 packages_router = APIRouter()
 
 
 @packages_router.post("/packages/", tags=["packages"])
-async def check_health_of_packages():
-    return bl.send_request_to_healthy()
+async def check_health_of_packages(packages_list: models.Packages):
+    return bl.send_request_to_healthy(packages_list)
