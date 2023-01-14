8724585c2728f772b4f0ebc0d0922e95fe8a7cd4
Hadar Nir <hadarnir33@gmail.com>
2023-01-14 22:39:00 +0200

    Change function name

--- services/resty/app/routers/packages.py
+++ services/resty/app/routers/packages.py
@@ -7,5 +7,5 @@ packages_router = APIRouter()
 
 
 @packages_router.post("/packages/", tags=["packages"])
-async def check_health_of_packages(packages_list: models.Packages):
+async def send_request_to_healthy(packages_list: models.Packages):
     return bl.send_request_to_healthy(packages_list)
