821fcaf53c8ae6b07202f8cb1eca36a53c4587ad
Hadar Nir <hadarnir33@gmail.com>
2023-01-15 22:43:32 +0200

    Change resty role

--- services/resty/app/models/packages.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class Packages(BaseModel):
-    packages_names_list: list
--- services/resty/app/routers/__init__.py
+++ services/resty/app/routers/__init__.py
@@ -1,1 +1,1 @@
-from .packages import packages_router
+from .healthy import packages_router
